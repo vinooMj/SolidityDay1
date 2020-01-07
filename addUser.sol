139ac99fa4eaceee795026bc180046e0646413a2
VinothShanmugam <31066524+vinooMj@users.noreply.github.com>
2020-01-07 16:24:41 +0530

    Create addUser.sol

---
+++ addUser.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.12;
+
+contract AddUser{
+    
+    // array
+    Person[] public people;
+    uint256 public count;
+    
+    
+    struct Person{
+        string firstName;
+        string lastName;
+    }
+    
+    function addUser(string memory firstName, string memory lastName )public{
+        people.push(Person(firstName,lastName));
+        count++;
+    }
+    
+}
